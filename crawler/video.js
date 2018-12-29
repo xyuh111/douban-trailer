b1f5654a2dd0b693c4b3dfe7e6d51d5d30cff7e0
xie yuhui <xyuh111@qq.com>
2018-12-29 18:07:23 +0800

    feat: 新增预告片详情页视频和封面图爬取

---
+++ crawler/video.js
@@ -0,0 +1,66 @@
+const puppeteer = require('puppeteer')
+const base = 'https://movie.douban.com/subject/'
+const doubanId = '3878007'
+const videoBase = 'https://movie.douban.com/trailer/240074/'
+
+const sleep = time => new Promise(resolve => {
+		setTimeout(resolve, time)
+	}).then(res => {
+
+	}).catch(res => {
+
+	})
+;(async () => {
+	console.log('start visit the target page')
+
+	const browser = await puppeteer.launch({
+		args: ['--no-sandbox'],
+		dumpio: false
+	})
+
+	const page = await browser.newPage()
+
+	await page.goto(base + doubanId, {
+
+		waitUntil: 'networkidle2'
+	})
+	await sleep(1000)
+	const result = await page.evaluate(async () => {
+		var $ = window.$;
+		var it = $('.related-pic-video');
+		if (it && it.length > 0) {
+			var link = it.attr('href')
+			var cover = it.attr('style').slice(21, -1)
+			return {
+				link,
+				cover,
+			}
+		}
+		return {}
+	});
+	let video;
+	if (result.link) {
+		await page.goto(result.link, {
+			waitUntil: "networkidle2"
+		})
+		await sleep(2000)
+		video = await page.evaluate(() => {
+			var $ = window.$
+			var it = $('source')
+			if (it && it.length > 0) {
+				return it.attr('src')
+			}
+			return ''
+		})
+	}
+
+	const data = {
+		video,
+		doubanId,
+		cover: result.cover,
+	}
+
+	browser.close()
+	process.send(data)
+	process.exit(0)
+})()
