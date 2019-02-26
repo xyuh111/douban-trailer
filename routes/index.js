31edd878661ce67adf8ded3fea00263c992f0982
xie yuhui <xyuh111@qq.com>
2019-02-26 20:24:12 +0800

    首次配置 koa-router

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const Router = require('koa-router')
+const mongoose = require('mongoose')
+const router = new Router()
+
+router.get('/movies/all', async(ctx,next) =>{
+    const Movie = mongoose.model('Movie')
+    const movies = await Movie.find({}).sort({    //时间 从最近到最远排序
+        'mete.createdAt': -1
+    })
+
+    //body 直接返回
+    ctx.body = {movies}
+})
+router.get('/movies/detail/:id', async(ctx,next) =>{
+    const Movie = mongoose.model('Movie')
+    const id = ctx.params.id;
+    const movies = await Movie.findOne({_id:id})
+    //body 直接返回
+    ctx.body = {movies}
+})
+
+module.exports = router;
